2f2283f3ef58c0d44c0678fae44ae6fa192ead32
jeetu-mj <jeetu.mj123@gmail.com>
2025-02-14 16:05:15 +0530

    Update inventory code

--- src/config/constants.js
+++ src/config/constants.js
@@ -1,4 +1,4 @@
-export const API_BASE_URL = 'http://localhost:3010/api';
+export const API_BASE_URL = 'https://inventory-zmsp.onrender.com/api';
 
 // live Url https://inventory-zmsp.onrender.com
 // local http://localhost:3010
