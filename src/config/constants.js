eef66baf92d7da13d2b0071490bc506a87d7f8ee
jeetu-mj <jeetu.mj123@gmail.com>
2025-02-15 09:21:16 +0530

    constant updte

--- src/config/constants.js
+++ src/config/constants.js
@@ -1,4 +1,4 @@
-export const API_BASE_URL = 'https://inventory-zmsp.onrender.com/api';
+export const API_BASE_URL = 'https://google.php/api';
 
 // live Url https://inventory-zmsp.onrender.com
 // local http://localhost:3010
